77ad0be2ed8e95862761e406253a02d664d3ebb9
kdschneider <ks@konstantin-schneider.eu>
2022-01-29 20:04:04 +0100

    Minor changes.

--- analysis/test_01.R
+++ analysis/test_01.R
@@ -81,7 +81,7 @@ test_01_plot_01 <-
 ## plot 2 ----
 plot_sd <- function(data, x) {
 
-  p <-
+  data <-
     data |>
     mutate(
       across(length:extra_ins, as_factor),
@@ -101,26 +101,22 @@ plot_sd <- function(data, x) {
     unnest(measurement) |>
     group_by({{ x }}) |>
     summarise(
-      median_volts = median(volts),
-      standard_deviation = sd(volts),
-      standard_error = plotrix::std.error(volts)
-    ) |>
+      standard_deviation = sd(volts)
+    )
+
+  p <-
+    data |>
     ggplot() +
-    aes(
-      x = {{ x }},
-      y = median_volts
-    ) +
-    geom_errorbar(
+    geom_col(
       aes(
-        y = median_volts,
-        ymin = median_volts - standard_deviation,
-        ymax = median_volts + standard_deviation
+        x = {{ x }},
+        y = standard_deviation
       ),
-      width = 0.25
+      fill = "grey",
+      color = "black"
     ) +
-    geom_point() +
-    labs(y = "Mittlere Spannung") +
-    ylim(-2.6, 2.3) +
+    labs(y = "σ") +
+    ylim(0, 2.5) +
     theme(axis.title.x = element_blank())
 
   p
@@ -134,77 +130,77 @@ p2 <- plot_sd(test_01_data, iso_traf) +
   labs(subtitle = "Trenntrafo") +
   theme(axis.title.y = element_blank())
 
-p3 <- plot_sd(test_01_data, ground) +
-  labs(subtitle = "Erdung")
+p3 <- plot_sd(test_01_data, extra_ins) +
+  labs(subtitle = "Extra Isolierung")
 
-p4 <- plot_sd(test_01_data, extra_ins) +
-  labs(subtitle = "Extra Isolierung") +
+p4 <- plot_sd(test_01_data, ground) +
+  labs(subtitle = "Erdung") +
   theme(axis.title.y = element_blank())
 
 test_01_plot_02 <-
   (p1 + p2) / (p3 + p4) &
-  plot_annotation(title = "Mittlere Spannung mit Standardabweichung")
+  plot_annotation(title = "Mittlere Standardabweichung")
 
 remove(p1, p2, p3, p4)
 
 
 ## plot 3 ----
+plot_pp <- function(data, x) {
+
+  p <-
+    data |>
+    mutate(
+      across(length:extra_ins, as_factor),
+      length = fct_relevel(length, "1", "2", "3") |>
+        fct_recode("1m" = "1", "2m" = "2", "3m" = "3"),
+      iso_traf = fct_relevel(iso_traf, "TRUE", "FALSE") |>
+        fct_recode("Ja" = "TRUE", "Nein" = "FALSE"),
+      ground = fct_relevel(ground, "TRUE", "FALSE") |>
+        fct_recode("Ja" = "TRUE", "Nein" = "FALSE"),
+      extra_ins = fct_relevel(extra_ins, "FALSE", "single", "double") |>
+        fct_recode(
+          "keine" = "FALSE",
+          "einfach" = "single",
+          "doppelt" = "double"
+        )
+    ) |>
+    unnest(measurement) |>
+    group_by(filename, {{ x }}) |>
+    summarise(
+      max_volts = max(volts),
+      min_volts = min(volts),
+      delta_volts = max_volts - min_volts
+    ) |>
+    ggplot() +
+    aes(
+      x = {{ x }},
+      y = delta_volts
+    ) +
+    geom_boxplot() +
+    labs(y = "Peak-to-Peak Spannung") +
+    theme(axis.title.x = element_blank())
+
+  p
+
+}
+
+p1 <- plot_pp(test_01_data, length) +
+  labs(subtitle = "Kabellänge")
+
+p2 <- plot_pp(test_01_data, iso_traf) +
+  labs(subtitle = "Trenntrafo") +
+  theme(axis.title.y = element_blank())
+
+p3 <- plot_pp(test_01_data, extra_ins) +
+  labs(subtitle = "Extra Isolierung")
+
+p4 <- plot_pp(test_01_data, ground) +
+  labs(subtitle = "Erdung") +
+  theme(axis.title.y = element_blank())
+
 test_01_plot_03 <-
-  test_01_data |>
-  mutate(
-    iso_traf = as.numeric(iso_traf),
-    ground = as.numeric(ground),
-    extra_ins = case_when(
-      extra_ins == "FALSE" ~ 0,
-      extra_ins == "single" ~ 1,
-      extra_ins == "double" ~ 2
-    )
-  ) |>
-  mutate(
-    across(
-      length:extra_ins,
-      ~scales::rescale(.x, to = c(-1,1))
-    )
-  ) |>
-  unnest(measurement) |>
-  pivot_longer(
-    cols = c(
-      length,
-      ground,
-      iso_traf,
-      extra_ins
-    )
-  ) |>
-  group_by(filename, name, rep, value) |>
-  summarise(
-    volts_max = max(volts),
-    volts_min = min(volts),
-    volts_delta = volts_max - volts_min,
-  ) |>
-  group_by(name, value) |>
-  mutate(volts_median = median(volts_delta)) |>
-  mutate(
-    name = as_factor(name) |>
-      fct_recode(
-        "Kabellänge" = "length",
-        "Erdung" = "ground",
-        "Trenntrafo" = "iso_traf",
-        "Extra Schirmung" = "extra_ins"
-      )
-  ) |>
-  # plot
-  ggplot() +
-  aes(x = value) +
-  geom_point(aes(y = volts_delta), size = 0.5) +
-  geom_point(aes(y = volts_median)) +
-  geom_line(aes(y = volts_median)) +
-  scale_x_continuous(n.breaks = 3) +
-  labs(
-    title = "Effektplots",
-    y = "Mittlere Peak-to-peak Spannung [V]"
-  ) +
-  facet_wrap(facets = vars(name)) +
-  theme(axis.title.x = element_blank())
+  (p1 + p2) / (p3 + p4) &
+  plot_annotation(title = "Peak-to-Peak Spannung")
 
 # write ----
 save(
