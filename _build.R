b5271a262b71c4f3e910f3527b355a3139ec6daf
kdschneider <ks@konstantin-schneider.eu>
2022-01-31 11:08:35 +0100

    Fix a analysis error.

--- _build.R
+++ _build.R
@@ -1,3 +1,25 @@
+# build analysis
+files <- list.files(
+  path = here::here("analysis"),
+  pattern = "*.Rmd",
+  full.names = TRUE
+)
+
+purrr::walk(
+  .x = files,
+  .f = function(x) {
+    rmarkdown::render(
+      input = x,
+      output_format = prettydoc::html_pretty(
+        theme = "cayman",
+        math = "mathjax",
+        keep_md = FALSE,
+        self_contained = TRUE
+      )
+    )
+  }
+)
+
 # build book
 bookdown::render_book(
   input = here::here(),
